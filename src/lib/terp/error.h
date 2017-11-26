b8c7c3a74bcb55fbdf2089270d8fb85c100539cd
Cameron Reikes <cameronreikes@gmail.com>
2017-11-26 01:44:14 -0800

    Added fatal error commands

--- src/lib/terp/error.h
+++ src/lib/terp/error.h
@@ -3,6 +3,10 @@
 
 void print_error( const char * to_print, const char * filename, int line );
 
+void print_fatal_error( const char * to_print, const char * filename, int line );
+
+#define fatal_error(x) print_fatal_error(x, __FILE__, __LINE__)
+
 #define error(x) print_error(x, __FILE__, __LINE__ )
 
 #endif
