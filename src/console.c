5544a7aa266fbbc4e6246781c8a97bb42004dd72
Cameron Reikes <cameronreikes@gmail.com>
2017-11-25 20:58:41 -0800

    Deleted everything, starting over

--- src/console.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "console.h"
-
-void print_color( int font_color, char * to_print ) {
-
-}
--- src/console.h
+++
@@ -1,6 +0,0 @@
-#ifndef H_CONSOLE
-#define H_CONSOLE
-
-const char c_red = 31;
-
-#endif
