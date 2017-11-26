404d2980b66038bed56d431fe6e6f7334e772ef7
Cameron Reikes <cameronreikes@gmail.com>
2017-11-26 13:23:01 -0800

    Added the ability to format errors

--- src/lib/error.c
+++ src/lib/error.c
@@ -10,3 +10,28 @@ void print_fatal_error( const char * to_print, const char * filename, int line )
   fprintf( stderr, "\033[1;33m\033[4m⚠ FATAL ERROR⚠ - \033[0m\033[1;31mERROR ON LINE %d IN FILE %s:\033[21;37m %s\033[0m\n", line, filename, to_print );
   return;
 }
+
+// Without the ending, used to print variables
+void print_partial_fatal_error( const char * to_print, const char * filename, int line ) {
+  fprintf( stderr, "\033[1;33m\033[4m⚠ FATAL ERROR⚠ - \033[0m\033[1;31mERROR ON LINE %d IN FILE %s:\033[21;37m %s", line, filename, to_print );
+  return;
+}
+
+void append_fatal_error( void * to_append ) {
+  if( to_append == NULL ) {
+    fatal_error("to_append in append fatal error is null");
+  }
+  switch( sizeof(*to_append) ) {
+    case sizeof(int):
+      printf("%d", *to_append);
+      break;
+    case sizeof(float):
+      printf("%f", *to_append);
+    default:
+      printf("UNKNOWN TYPE");
+  }
+}
+
+void end_error() {
+  printf("\033[0m\n");
+}
