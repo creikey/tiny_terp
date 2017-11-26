8d3af71ca47ff2766f48e2fb1513dbeba4f53855
Cameron Reikes <cameronreikes@gmail.com>
2017-11-26 01:45:21 -0800

    Now it prints the input file

--- src/compiler/main.c
+++ src/compiler/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "terp/error.h"
+#include "terp/file.h"
 
 int main( int argc, char ** argv ) {
   printf( "Hello, world\n" );
+  char * to_print = get_file( "Makefile" );
   error( "bruh" );
+  printf( "File is: %s\n", to_print );
 }
