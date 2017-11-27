6a7f7568a2d4d0e0e04734c1e897c4bfa52b888f
Cameron Reikes <cameronreikes@gmail.com>
2017-11-26 20:18:59 -0800

    Formatted errors

--- src/compiler/terp/error.h
+++ src/compiler/terp/error.h
@@ -5,7 +5,15 @@ void print_error( const char * to_print, const char * filename, int line );
 
 void print_fatal_error( const char * to_print, const char * filename, int line );
 
+void print_partial_fatal_error( const char * to_print, const char * filename, int line );
+
+void end_error();
+
+#define append_fatal_error(x) printf( x );
+
 #define fatal_error(x) print_fatal_error(x, __FILE__, __LINE__)
+
+#define format_fatal_error(x) print_partial_fatal_error(x, __FILE__, __LINE__)
 
 #define error(x) print_error(x, __FILE__, __LINE__ )
 
