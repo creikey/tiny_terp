93b422886add8d103fcdda9b32d18d2f305b69a1
Cameron Reikes <cameronreikes@gmail.com>
2017-11-26 20:17:28 -0800

    Variables have types

--- src/lib/terp/variables.h
+++ src/lib/terp/variables.h
@@ -1,7 +1,14 @@
 #ifndef H_VARIABLES
 #define H_VARIABLES
 
+typedef enum variable_data {
+  integer,
+  floating,
+  strn
+} var_type;
+
 typedef struct _variable_block {
+  var_type data_type;
   void * data;
   char * var_name;
   struct _variable_block * next_variable;
@@ -9,5 +16,7 @@ typedef struct _variable_block {
 } variable_block;
 
 void * get_variable_val_int( variable_block * vars, char * var_name );
+
+void debug_variables( variable_block * vars );
 
 #endif
