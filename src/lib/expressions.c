1239610a585c427b229d0e7a44ff706841a8e676
Cameron Reikes <cameronreikes@gmail.com>
2017-11-26 20:18:29 -0800

    Small bug fixes

--- src/lib/expressions.c
+++ src/lib/expressions.c
@@ -1,5 +1,9 @@
 // Parses bytecode expressions
 #include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include <string.h>
+#include "terp/error.h"
 #include "terp/expressions.h"
 #include "terp/bytecode.h"
 #include "terp/variables.h"
@@ -7,9 +11,12 @@
 int parse_expression( variable_block * vars, char * byte_stream, int position ) {
   int expr_type = -1;
   bool end_of_expr = false;
-  int curchar = 0
-  int left_var = -1;
-  int right_var = -1;
+  int curchar = 0;
+  int left_var;
+  int right_var;
+  // For searching for vars
+  bool found_var;
+  // Loop for the expression
   while( end_of_expr == false ) {
     switch( byte_stream[position+curchar] ) {
       case END_EXPR:
@@ -24,14 +31,22 @@ int parse_expression( variable_block * vars, char * byte_stream, int position ) 
       case EQUAL_TO:
         expr_type = EQUAL_TO;
         break;
+        // If there's another expression
+      case BEGIN_EXPR:
+        // Recursively call it
+        if( expr_type == -1 ) {
+          left_var = parse_expression(vars, byte_stream, position+curchar);
+        } else {
+          right_var = parse_expression( vars, byte_stream, position+curchar );
+        }
       // If there's a clause to get a variable
       case GET_VARIABLE:
         // If it's found the var yet
-        bool found_var = false;
+        found_var = false;
         // The cursor that's searching for the end of the var
-        int varchar = position+cur;
+        int varchar = position+curchar;
         // The starting position of the variable's string to pull
-        int varstart = position+cur+1;
+        int varstart = position+curchar+1;
         // The size of the variable's string to pull
         size_t name_size = 0;
         // While it hasn't found the var
@@ -58,13 +73,15 @@ int parse_expression( variable_block * vars, char * byte_stream, int position ) 
         to_var[name_size] = '\0';
         void * var_test = get_variable_val_int( vars, to_var );
         if( var_test == NULL ) {
-          fatal_error( "variable wasn't an integer" );
+          format_fatal_error( "couldn't find variable for expression, vars: " );
+          debug_variables(vars);
+          end_error();
           break;
         } else {
           if( expr_type == -1 ) {
-            left_var = *var_test;
+            left_var = *(int*)var_test;
           } else {
-            right_var = *var_test;
+            right_var = *(int*)var_test;
           }
         }
         break;
