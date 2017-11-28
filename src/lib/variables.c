915d78e105557c3e55debfb410811cb4b2844d47
Cameron Reikes <cameronreikes@gmail.com>
2017-11-27 23:42:23 -0800

    Improved code look

--- src/lib/variables.c
+++ src/lib/variables.c
@@ -72,7 +72,7 @@ void * get_variable_val_int( variable_block * vars, char * var_name ) {
     found_var = true;
   }
   if( found_var ) {
-    return *((int*)cur->data);
+    return *(int*)cur->data;
   }
 }
 
