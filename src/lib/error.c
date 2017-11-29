7a9ce92a38c0b5abd7074e9084b9671d094c1bb9
Cameron Reikes <cameronreikes@gmail.com>
2017-11-28 20:52:36 -0800

    Re do most of the errors

--- src/lib/error.c
+++ src/lib/error.c
@@ -1,33 +1,66 @@
 #include <stdio.h>
 #include "terp/error.h"
 
-void print_error( const char * to_print, const char * filename, int line ) {
-  fprintf( stderr, "\033[1;31mERROR ON LINE \033[1;33m\033[4m%d\033[0m \033[1;31mIN FILE \033[1;33m\033[4m%s\033[0m:\033[21;37m %s\033[0m\n", line, filename, to_print );
+void print_error( const char * to_print, const char * filename, int line, bool close_message ) {
+  fprintf( stderr, "\033[1;31mERROR ON LINE \033[1;33m\033[4m%d\033[0m \033[1;31mIN FILE \033[1;33m\033[4m%s\033[0m:\033[21;37m %s", line, filename, to_print );
+  if( close_message ) {
+    fprintf( stderr, "\033[0m\n");
+  }
   return;
 }
 
-void print_fatal_error( const char * to_print, const char * filename, int line ) {
-  fprintf( stderr, "\033[1;33m\033[4m⚠ FATAL ERROR⚠ - \033[0m\033[1;31mERROR ON LINE %d IN FILE %s:\033[21;37m %s\033[0m\n", line, filename, to_print );
+void print_fatal_error( const char * to_print, const char * filename, int line, bool close_message ) {
+  fprintf( stderr, "\033[1;33m\033[4m⚠ FATAL ERROR⚠ - \033[0m\033[1;31mERROR ON LINE %d IN FILE %s:\033[21;37m %s", line, filename, to_print );
+  if( close_message ) {
+    fprintf( stderr, "\033[0m\n");
+  }
   return;
+}
+
+void print_message( const char * to_print, const char * filename, int line, bool close_message ) {
+  fprintf( stderr, "\033[1;33mnon-fatal log at %d in %s:\033[0m%s", line, filename, to_print );
+  if( close_message ) {
+    fprintf( stderr, "\n");
+  }
+  return;
+}
+
+void print_general_string( const char * to_print ) {
+  fprintf( stderr, "%s", to_print );
+}
+
+void print_general_char( char to_print ) {
+  fprintf(stderr, "%c\n", to_print );
+}
+
+void print_general_float( float to_print ) {
+  fprintf(stderr, "%f", to_print);
+}
+
+void print_general_int( int to_print ) {
+  fprintf(stderr, "%d", to_print);
+}
+
+void close_general_error() {
+  fprintf( stderr, "\033[0m\n");
 }
 
 // Without the ending, used to print variables
-void print_partial_fatal_error( const char * to_print, const char * filename, int line ) {
+/*void print_partial_fatal_error( const char * to_print, const char * filename, int line ) {
   fprintf( stderr, "\033[1;33m\033[4m⚠ FATAL ERROR⚠ - \033[0m\033[1;31mERROR ON LINE %d IN FILE %s:\033[21;37m %s", line, filename, to_print );
   return;
-}
+}*/
 
-void end_error() {
+/*void end_error() {
   printf("\033[0m\n");
-}
+}*/
 
-void print_message( const char * to_print, const char * filename, int line ) {
-  fprintf( stderr, "\033[1;33mnon-fatal log at %d in %s:\033[0m %s\n", line, filename, to_print );
-  return;
-}
 
-void print_partial_message( const char * to_print, const char * filename, int line ) {
-  fprintf( stderr, "\033[1;33mnon-fatal log at %d in %s:\033[0m %s", line, filename, to_print );
+
+
+
+/*void print_partial_message( const char * to_print, const char * filename, int line ) {
+  fprintf( stderr, "\033[1;33mnon-fatal log at line %d in %s:\033[0m %s", line, filename, to_print );
   return;
 }
 
@@ -47,4 +80,4 @@ void continue_partial_message( const char * to_print ) {
 
 void end_partial_message() {
   fprintf( stderr, "\n" );
-}
+}*/
