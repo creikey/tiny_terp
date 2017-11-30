7b71d3c961c0be75be05e8454d10f11225c56159
Cameron Reikes <cameronreikes@gmail.com>
2017-11-29 20:49:44 -0800

    makes sure something's bytecode

--- src/lib/bytecode.c
+++ src/lib/bytecode.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "terp/bytecode.h"
 
 const int possible_bytes[] = { BEGIN_EXPR, END_EXPR, VARIABLE_START, VARIABLE_END, GET_VARIABLE, GREATER_THAN, LESS_THAN, EQUAL_TO, SET_VARIABLE };
+
+bool is_bytecode( int in_byte ) {
+  size_t bytecode_size = sizeof(possible_bytes);
+  for( size_t i = 0; i < bytecode_size; i++ ) {
+    if( possible_bytes[i] == in_byte ) {
+      return true;
+    }
+  }
+  return false;
+}
