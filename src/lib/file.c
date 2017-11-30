dbfd6cd29a3845ea67eb9ae6e17aad9fc3638ef0
Cameron Reikes <cameronreikes@gmail.com>
2017-11-29 23:56:38 -0800

    Re doing reading file functions

--- src/lib/file.c
+++ src/lib/file.c
@@ -3,7 +3,9 @@
 #include "terp/error.h"
 #include "terp/file.h"
 
-char * get_file( const char * file_path ) {
+
+
+/*char * get_file( const char * file_path ) {
   // Declares the file pointer
   FILE * to_open = NULL;
   // Opens it
@@ -28,4 +30,35 @@ char * get_file( const char * file_path ) {
   fclose( to_open );
   // Returns the new pointer
   return to_return;
+}*/
+
+int * get_file_bytestream( const char * file_path ) {
+  // The variables
+  int * to_return; // the bytestream to return
+  int file_len; // the length of the input file
+  FILE * to_open; // the file pointer to open
+  // Opens it
+  to_open = fopen( file_path, "r" );
+  // Gets file length
+  file_len = get_file_len( to_open );
+  // Mallocs the memory needed to return
+  to_return = malloc( sizeof(int) * file_len );
+  return to_return;
+}
+
+unsigned int get_file_len( FILE * in_fp ) {
+  if( in_fp == NULL ) {
+    // No file given
+    fatal_error( "in file was null", false );
+    return 0;
+  } else {
+    // Go to the end
+    fseek( in_fp, 0, SEEK_END );
+    // Get file length
+    unsigned int to_return = ftell( in_fp );
+    // Go to the beginning
+    rewind( in_fp );
+    // Return file length
+    return to_return;
+  }
 }
