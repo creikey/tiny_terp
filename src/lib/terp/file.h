6ebe86c6ae6f3108d6adcba0edb97e8c060350b4
Cameron Reikes <cameronreikes@gmail.com>
2017-11-29 23:58:29 -0800

    Function to get bytecode length

--- src/lib/terp/file.h
+++ src/lib/terp/file.h
@@ -5,6 +5,8 @@ typedef struct _FILE FILE;
 
 //char * get_file( const char * file_path );
 
+size_t get_bytecode_len( int * in_bytes );
+
 int * get_file_bytestream( const char * file_path );
 
 unsigned int get_file_len( FILE * in_fp );
